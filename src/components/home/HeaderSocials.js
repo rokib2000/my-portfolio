5e2685e2514e7338a3900c54ed9ef03026c2d279
rokib2000 <rokib0w1@gmail.com>
2022-12-17 01:36:12 +0600

    Added home page

--- src/components/home/HeaderSocials.js
+++ src/components/home/HeaderSocials.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const HeaderSocials = () => {
-  return <div className="home_socials"></div>;
+  return (
+    <div className="home_socials">
+      <a href="https://www.linkedin.com/in/md-nazrul-islam-rokib/" className="home_socials-link" target="_blank">
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+      <a href="https://github.com/rokib2000" className="home_socials-link" target="_blank">
+        <i class="fa-brands fa-github"></i>
+      </a>
+      <a href="https://twitter.com/rr_okib" className="home_socials-link" target="_blank">
+        <i class="fa-brands fa-twitter"></i>
+      </a>
+      <a href="https://www.facebook.com/rokib2000/" className="home_socials-link" target="_blank">
+        <i class="fa-brands fa-facebook"></i>
+      </a>
+    </div>
+  );
 };
 
 export default HeaderSocials;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,36 @@
 import React from "react";
 import "./Home.css";
 import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
+import Shapes from "./Shapes";
 
 const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
         <img src={Me} alt="" className="home_img" />
-        <h1 className="home_name">Md Nazrul Islam</h1>
+        <h1 className="home_name">Md Nazrul Islam Rokib</h1>
         <span className="home_education">I'm a Front-End developer</span>
+
+        <HeaderSocials />
+
+        {/* <a href="#contact" className="btn">
+          Hire Me
+        </a> */}
+
+        <a
+          href="https://drive.google.com/file/d/1nRQjpvTKSQMb5bfEeNCw74QywA5JccH4/view?usp=share_link"
+          className="btn"
+          target="_blank"
+        >
+          Download Resume
+        </a>
+
+        <ScrollDown />
       </div>
+
+      {/* <Shapes /> */}
     </section>
   );
 };
