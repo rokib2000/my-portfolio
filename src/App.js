d8d53bb5f0fd7261a3104d6d361ce7cb4ac44e6e
rokib2000 <rokib0w1@gmail.com>
2022-12-16 23:57:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./components/about/About";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
+import Home from "./components/home/Home";
+import Portfolio from "./components/portfolio/Portfolio";
+import Resume from "./components/resume/Resume";
+import Sidebar from "./components/sidebar/Sidebar";
+import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <main>
+        <Home />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../../assets/logo.svg";
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <a href="#home">R</a>
+    </aside>
+  );
+};
+
+export default Sidebar;
