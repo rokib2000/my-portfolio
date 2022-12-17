023ce86e812003e7ca476b68c0e8449148eeb65f
rokib2000 <rokib0w1@gmail.com>
2022-12-17 15:19:22 +0600

    added portfolio section

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Portfolio.css";
+import Items from "./Menu";
+import WorkCard from "./WorkCard";
 
 const Portfolio = () => {
   return (
     <section className="work container section" id="work">
       <h2 className="section_title">Projects</h2>
+
+      <div className="work_container grid">
+        {Items.map((item) => {
+          return <WorkCard key={item.id} item={item} />;
+        })}
+      </div>
     </section>
   );
 };
---
+++ src/components/portfolio/WorkCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const WorkCard = ({ item }) => {
+  const { image, title, description, github_server, github_client, live_website } = item;
+  return (
+    <div className="work_card">
+      <div className="work_thumbnail">
+        <img src={image} alt="" className="work_image" />
+        <div className="work_mask"></div>
+      </div>
+
+      <a href="#" alt className="work_details">
+        See Details
+      </a>
+      <h3 className="work_title">{title}</h3>
+      <a href="#" className="work_button1">
+        <i className="icon-link work_button-icon"></i>
+      </a>
+      <a href="#" className="work_button2">
+        <i className="icon-social-github work_button-icon"></i>
+      </a>
+      <a href="#" className="work_button3">
+        <i className="icon-social-github work_button-icon"></i>
+      </a>
+    </div>
+  );
+};
+
+export default WorkCard;
