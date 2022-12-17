be67d0f27c70dbab0ac56ef77801453857b1f346
rokib2000 <rokib0w1@gmail.com>
2022-12-17 16:01:45 +0600

    added contact section

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
       <main className="main">
         <Home />
         <About />
-        <Resume />
         <Portfolio />
-        <Testimonials />
-        <Blog />
+        <Resume />
+        {/* <Testimonials /> */}
+        {/* <Blog /> */}
         <Contact />
       </main>
     </>
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,45 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section className="contact container section" id="contact">
+      <h2 className="section_title">Get In Touch</h2>
+
+      <div className="contact_container grid">
+        <div className="contact_info">
+          <h3 className="contact_title">Let's talk about everything!</h3>
+          <p className="contact_details">Don't like forms? Send me an email.</p>
+        </div>
+
+        <form action="" className="contact_form">
+          <div className="contact_form-group">
+            <div className="contact_form-div">
+              <input type="text" className="contact_form-input" placeholder="Enter your name" />
+            </div>
+            <div className="contact_form-div">
+              <input type="email" className="contact_form-input" placeholder="Enter your email" />
+            </div>
+          </div>
+
+          <div className="contact_form-div">
+            <input type="text" className="contact_form-input" placeholder="Enter your subject" />
+          </div>
+          <div className="contact_form-div contact_form-area">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className="contact_form-input"
+              placeholder="Write your message"
+            ></textarea>{" "}
+          </div>
+
+          <button className="btn">Send Message</button>
+        </form>
+      </div>
+    </section>
   );
 };
 
